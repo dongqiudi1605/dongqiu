2b0dddc77759352efc6dc88fcff4899baebc0f92
hongkun1992 <1778629094@qq.com>
2016-07-04 19:46:44 +0800

    开始

---
+++ app/src/main/java/com/sd/dongqiu/MainActivity.java
@@ -0,0 +1,12 @@
+package com.sd.dongqiu;
+
+import android.os.Bundle;
+
+public class MainActivity extends myActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
