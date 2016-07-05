afef2913b3865c0ffbfe99f1bc46db00d4a9c660
lhmz <mo601113733@qq.com>
2016-07-05 11:31:24 +0800

    53

---
+++ app/src/main/java/FiveFragment/fragment_quanzi.java
@@ -0,0 +1,22 @@
+package FiveFragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sd.dongqiu.R;
+
+/**
+ * Created by mr on 2016/7/5.
+ */
+public class fragment_quanzi extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        View view=inflater.inflate(R.layout.fragment_quanzi, container, false);
+        return view;
+    }
+}
